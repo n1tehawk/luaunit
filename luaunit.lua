76c5e9768012fdae831e716c23a1e2c6c7e0d362
NiteHawk <n1tehawk@users.noreply.github.com>
2016-07-29 11:11:07 +0200

    Convert default "margin_boost" into a fully optional "epsilon"
    The goal is NOT to modify the user-specified margin, unless explicitly
    requested. When setting an epsilon, the user may pass a (numeric)
    value directly, or simply use a boolean "true" to ask for the default
    EPSILON.

--- luaunit.lua
+++ luaunit.lua
@@ -1206,7 +1206,7 @@ function M.assertEquals(actual, expected, doDeepAnalysis)
     end
 end
 
-function M.almostEquals( actual, expected, margin, margin_boost )
+function M.almostEquals( actual, expected, margin, epsilon )
     if type(actual) ~= 'number' or type(expected) ~= 'number' or type(margin) ~= 'number' then
         error_fmt(3, 'almostEquals: must supply only number arguments.\nArguments supplied: %s, %s, %s',
             prettystr(actual), prettystr(expected), prettystr(margin))
@@ -1214,13 +1214,16 @@ function M.almostEquals( actual, expected, margin, margin_boost )
     if margin < 0 then
         error('almostEquals: margin must not be negative, current value is ' .. margin, 3)
     end
-    local realmargin = margin + (margin_boost or M.EPSILON)
-    return math.abs(expected - actual) <= realmargin
+    if epsilon then
+        epsilon = tonumber(epsilon) or M.EPSILON
+        return math.abs(expected - actual) <= margin + epsilon
+    end
+    return math.abs(expected - actual) <= margin
 end
 
-function M.assertAlmostEquals( actual, expected, margin )
+function M.assertAlmostEquals( actual, expected, margin, epsilon )
     -- check that two floats are close by margin
-    if not M.almostEquals(actual, expected, margin) then
+    if not M.almostEquals(actual, expected, margin, epsilon) then
         if not M.ORDER_ACTUAL_EXPECTED then
             expected, actual = actual, expected
         end
@@ -1244,9 +1247,9 @@ function M.assertNotEquals(actual, expected)
     fail_fmt(2, 'Received the not expected value: %s', prettystr(actual))
 end
 
-function M.assertNotAlmostEquals( actual, expected, margin )
+function M.assertNotAlmostEquals( actual, expected, margin, epsilon )
     -- check that two floats are not close by margin
-    if M.almostEquals(actual, expected, margin) then
+    if M.almostEquals(actual, expected, margin, epsilon) then
         if not M.ORDER_ACTUAL_EXPECTED then
             expected, actual = actual, expected
         end
